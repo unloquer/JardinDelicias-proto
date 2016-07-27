5d4ca246e2974f2e7ef75d510f88f2323365d03f
miguelitoVargas <miguelito.vargasf@gmail.com>
2016-07-27 16:34:21 -0500

    pagina inicio agregada, mapa de google-maps en enjambre,todo: pasar a leaflet la api de mapas

--- inspinia-ui/entorno/js/app.js
+++ inspinia-ui/entorno/js/app.js
@@ -1,98 +1,125 @@
-$(function() {
+$(document).ready(function(){
 
-    var container = $("#flot-line-chart-moving");
+   if(document.getElementById("flot-line-chart-moving") !== null){
+      $(function() {
 
-    // Determine how many data points to keep based on the placeholder's initial size;
-    // this gives us a nice high-res plot while avoiding more than one point per pixel.
+         var container = $("#flot-line-chart-moving");
 
-    var maximum = container.outerWidth() / 2 || 300;
+         // Determine how many data points to keep based on the placeholder's initial size;
+         // this gives us a nice high-res plot while avoiding more than one point per pixel.
 
-    //
+         var maximum = container.outerWidth() / 2 || 300;
 
-    var data = [];
+         //
 
-    function getRandomData() {
+         var data = [];
 
-        if (data.length) {
-            data = data.slice(1);
-        }
+         function getRandomData() {
 
-        while (data.length < maximum) {
-            var previous = data.length ? data[data.length - 1] : 50;
-            var y = previous + Math.random() * 10 - 5;
-            data.push(y < 0 ? 0 : y > 100 ? 100 : y);
-        }
+            if (data.length) {
+               data = data.slice(1);
+            }
 
-        // zip the generated y values with the x values
+            while (data.length < maximum) {
+               var previous = data.length ? data[data.length - 1] : 50;
+               var y = previous + Math.random() * 10 - 5;
+               data.push(y < 0 ? 0 : y > 100 ? 100 : y);
+            }
 
-        var res = [];
-        for (var i = 0; i < data.length; ++i) {
-            res.push([i, data[i]])
-        }
+            // zip the generated y values with the x values
 
-        return res;
-    }
+            var res = [];
+            for (var i = 0; i < data.length; ++i) {
+               res.push([i, data[i]])
+            }
 
-    series = [{
-        data: getRandomData(),
-        lines: {
-            fill: true
-        }
-    }];
+            return res;
+         }
+
+         series = [{
+            data: getRandomData(),
+            lines: {
+               fill: true
+            }
+         }];
 
 
-    var plot = $.plot(container, series, {
-        grid: {
+         var plot = $.plot(container, series, {
+            grid: {
 
-            color: "#999999",
-            tickColor: "#D4D4D4",
-            borderWidth:0,
-            minBorderMargin: 20,
-            labelMargin: 10,
-            backgroundColor: {
-                colors: ["#ffffff", "#ffffff"]
-            },
-            margin: {
-                top: 8,
-                bottom: 20,
-                left: 20
-            },
-            markings: function(axes) {
-                var markings = [];
-                var xaxis = axes.xaxis;
-                for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
-                    markings.push({
+               color: "#999999",
+               tickColor: "#D4D4D4",
+               borderWidth:0,
+               minBorderMargin: 20,
+               labelMargin: 10,
+               backgroundColor: {
+                  colors: ["#ffffff", "#ffffff"]
+               },
+               margin: {
+                  top: 8,
+                  bottom: 20,
+                  left: 20
+               },
+               markings: function(axes) {
+                  var markings = [];
+                  var xaxis = axes.xaxis;
+                  for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+                     markings.push({
                         xaxis: {
-                            from: x,
-                            to: x + xaxis.tickSize
+                           from: x,
+                           to: x + xaxis.tickSize
                         },
                         color: "#fff"
-                    });
-                }
-                return markings;
+                     });
+                  }
+                  return markings;
+               }
+            },
+            colors: ["#1ab394"],
+            xaxis: {
+               tickFormatter: function() {
+                  return "";
+               }
+            },
+            yaxis: {
+               min: 0,
+               max: 110
+            },
+            legend: {
+               show: true
             }
-        },
-        colors: ["#1ab394"],
-        xaxis: {
-            tickFormatter: function() {
-                return "";
-            }
-        },
-        yaxis: {
-            min: 0,
-            max: 110
-        },
-        legend: {
-            show: true
-        }
-    });
+         });
 
-    // Update the random dataset at 25FPS for a smoothly-animating chart
+         // Update the random dataset at 25FPS for a smoothly-animating chart
 
-    setInterval(function updateRandom() {
-        series[0].data = getRandomData();
-        plot.setData(series);
-        plot.draw();
-    }, 40);
+         setInterval(function updateRandom() {
+            series[0].data = getRandomData();
+            plot.setData(series);
+            plot.draw();
+         }, 40);
 
+      });
+   }
+   if(document.getElementById("map1") !== null){
+
+      //when window has finished loading google map
+      google.maps.event.addDomListener(window, 'load',init);
+
+      function init(){
+
+         var mapOptions = {
+            zoom: 11,
+            center: new google.maps.LatLng(6.1506,-75.3348),
+            // Style for Google Maps
+            styles: [{"stylers":[{"hue":"#18a689"},{"visibility":"on"},{"invert_lightness":true},{"saturation":40},{"lightness":10}]}]
+         };
+
+         //get element for map
+         var mapElement = document.getElementById('map1');
+
+         //create the map
+         var swarm_map = new google.maps.Map(mapElement,mapOptions);
+      }
+
+   }
 });
